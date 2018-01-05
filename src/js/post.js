61e861a2c9f8d88118cc6e3af21a35b8c28560b7
Sergio Pomárico <sergiodavid21@gmail.com>
2018-01-05 14:14:59 -0500

    facebook buttom shared

--- src/js/post.js
+++ src/js/post.js
@@ -10,8 +10,24 @@ const Post = () => {
 				})
 			})
 		},
+		shared: function () {
+		},
+		facebook: function() {
+			$(document).ready(() =>{
+				$('#facebookShare').click(function() {
+					const url = $(this).data('href')
+					FB.ui({
+						method: 'share',
+						mobile_iframe: true,
+						href: url
+					}, function(response) {})
+				})
+			})
+		},
 		init: function() {
 			this.metadataIcon()
+			this.shared()
+			this.facebook()
 		}
 	}
 	post.init()
