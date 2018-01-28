0cc67e96756c9d1ed2dab10f4af9703e7b9f5d9c
Sergio Pomárico <sergiodavid21@gmail.com>
2018-01-28 12:22:31 -0500

    linkedin, whatsapp and api web shared, add comments with disqus

--- src/js/post.js
+++ src/js/post.js
@@ -11,6 +11,23 @@ const Post = () => {
 			})
 		},
 		shared: function () {
+			$(document).ready(() =>{
+				if (navigator.share) {
+					const share = $('#webShare')
+					const title = share.data('title')
+					const href = share.data('href')
+					share.click(function(e) {
+						e.preventDefault()
+
+						navigator.share({
+							url: href,
+							title: title
+						})
+
+						return false
+					})
+				}
+			})
 		},
 		facebook: function() {
 			$(document).ready(() =>{
@@ -32,15 +49,41 @@ const Post = () => {
 				const href = $('#twitterShare').data('href')
 				const url = intent+'text='+text+'&url='+href+'&via=codeoweb'	
 				$('#twitterShare').click(function(e) {
-					window.open(url, 'Twitter', 'height=360,width=480').focus()
+					window.open(url, 'Twitter', 'height=340,width=480').focus()
 				})
 			})
+		},
+		linkedin: function () {
+			$(document).ready( function() {
+				const intent = 'https://www.linkedin.com/shareArticle?mini=true&url'
+				const title = $('#linkedinShare').data('title')
+				const text = encodeURI(title)
+				const href = $('#linkedinShare').data('href')
+				const url = intent+href+'&title='+text+'&source=Codeo'
+				$('#linkedinShare').click(function(e) {
+					window.open(url, 'linkedin', 'height=340,width=480').focus()
+				})
+			})
+		},
+		whatsapp: function() {
+			$(document).ready( function() {
+				const whatsapp = $('#whatsappShare')
+				const intent = 'whatsapp://send?text='
+				const title = whatsapp.data('title')
+				const text = encodeURI(title)
+				const href = whatsapp.data('href')
+				const url = intent+text+' - '+ href
+				whatsapp.attr('href', url)
+			})
+			
 		},
 		init: function() {
 			this.metadataIcon()
 			this.shared()
 			this.facebook()
 			this.twitter()
+			this.linkedin()
+			this.whatsapp()
 		}
 	}
 	post.init()
