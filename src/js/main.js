7a77f11636521299591cba12d9dd4d2b633b1c76
Sergio Pomárico <sergiodavid21@gmail.com>
2018-03-04 13:35:48 -0500

    loader

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,14 @@ const Hero = require('./hero')
 const Dates = require('./date')
 const Post = require('./post')
 
+window.onload = function() {
+	$('#loading').delay(900).fadeOut('slow')
+}
+
 $(document).ready(function() {
 	Menu()
 	Hero()
 	Dates()
 	Post()
 })
+
